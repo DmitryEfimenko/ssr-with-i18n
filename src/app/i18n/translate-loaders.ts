d2335a87e929b2da2772ae41dfd9b370e6e204fd
Dmitry Efimenko <edmitry@amazon.com>
2019-08-04 14:32:02 -0700

    step 6

--- src/app/i18n/translate-loaders.ts
+++ src/app/i18n/translate-loaders.ts
@@ -7,6 +7,8 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 import { Observable, of } from 'rxjs';
 
+const i18nMap = require('../../assets/i18n/autogen/map.json');
+
 export class TranslateBrowserLoader implements TranslateLoader {
   constructor(
     private transferState: TransferState,
@@ -19,21 +21,27 @@ export class TranslateBrowserLoader implements TranslateLoader {
     const key = makeStateKey<any>('transfer-translate-' + lang);
     const data = this.transferState.get(key, null);
 
+    const suffix = `.${i18nMap[lang]}${this.suffix}`;
+
     // First we are looking for the translations in transfer-state, if none found, http load as fallback
     return data
       ? of(data)
-      : new TranslateHttpLoader(this.http, this.prefix, this.suffix).getTranslation(lang);
+      : new TranslateHttpLoader(this.http, this.prefix, suffix).getTranslation(lang);
   }
 }
 
 export class TranslateFSLoader implements TranslateLoader {
-  constructor(private transferState: TransferState, private prefix = './assets/i18n', private suffix = '.json') { }
+  constructor(
+    private transferState: TransferState,
+    private prefix = './assets/i18n/',
+    private suffix = '.json'
+  ) { }
 
   /**
    * Gets the translations from the server, store them in the transfer state
    */
   public getTranslation(lang: string): Observable<any> {
-    const path = join(__dirname, '../browser', this.prefix, `${lang}${this.suffix}`);
+    const path = join(__dirname, '../browser', this.prefix, `${lang}.${i18nMap[lang]}${this.suffix}`);
     const data = JSON.parse(readFileSync(path, 'utf8'));
 
     const key = makeStateKey<any>('transfer-translate-' + lang);
@@ -44,7 +52,8 @@ export class TranslateFSLoader implements TranslateLoader {
 }
 
 export function translateLoaderFactory(httpClient: HttpClient, transferState: TransferState, platform: any) {
+  const prefix = './assets/i18n/autogen/';
   return isPlatformBrowser(platform)
-    ? new TranslateBrowserLoader(transferState, httpClient)
-    : new TranslateFSLoader(transferState);
+    ? new TranslateBrowserLoader(transferState, httpClient, prefix)
+    : new TranslateFSLoader(transferState, prefix);
 }
